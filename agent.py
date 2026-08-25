3a63a4f34640e823c3e249cfc0da9e49675c4a62
hussaindadibrahimi296-ctrl <hussaindadibrahimi296@gmail.com>
2026-08-26 02:57:13 +0430

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 from sources import get_news
+from database import load_seen_news, save_seen_news
 
 
 def main():
@@ -7,10 +8,26 @@ def main():
 
     news = get_news()
 
-    print(f"Found {len(news)} news articles.")
-    print()
+    seen_news = load_seen_news()
+
+    new_news = []
 
     for item in news:
+        link = item["link"]
+
+        if link in seen_news:
+            continue
+
+        new_news.append(item)
+        seen_news.add(link)
+
+    save_seen_news(seen_news)
+
+    print(f"Total news found: {len(news)}")
+    print(f"New news: {len(new_news)}")
+    print()
+
+    for item in new_news:
         print(f"Source: {item['source']}")
         print(f"Title: {item['title']}")
         print(f"Link: {item['link']}")
